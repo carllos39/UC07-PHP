f7338358c2ce213b8825e0ad76e7ed466ff6812b
carllos39 <140573083+carllos39@users.noreply.github.com>
2025-04-29 22:20:26 -0300

    php

---
+++ 01/exercicio/formcliente.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="recebercliente.php" method="get">
+        <div>
+            <label for="nome">Nome :</label>
+            <input type="text" name="nome" id="nome">
+        </div>
+        <div>
+            <label for="email">Email :</label>
+            <input type="text" name="email" id="email">
+        </div>
+        <div>
+            <label for="telefone">Telefone:</label>
+            <input type="text" name="telefone" id="telefone">
+        </div>
+        <input type="submit" value="Enviar">
+    </form>
+    
+</body>
+</html>
