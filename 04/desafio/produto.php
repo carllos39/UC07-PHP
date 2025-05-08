70169cf9001147ef4c48833226c453e2d2cc6f32
carllos39 <carllos2005@outlook.com.br>
2025-05-08 02:05:38 -0300

    atualizar

--- 04/desafio/produto.php
+++ 04/desafio/produto.php
@@ -1,30 +1,8 @@
 <?php 
-$produto = $_GET['produto'];
+$nome = $_GET['nome'];
 $preco = $_GET['preco'];
 
-$produtos[]= [$produto, $preco];
-
-
+$produtos[]=[$nome,$preco];
 print_r($produtos);
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>lista</title>
-</head>
-<body>
-    <table>
-        <tr>
-            <th>Produto</th>
-            <th>Preço</th>
-        </tr>
-        <?php foreach($produtos as $pro){
-            
-        }
-    </table>
-    
-</body>
-</html>
