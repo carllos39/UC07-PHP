62299e8598ff1ba291b8ba4363fd5e8b9ab9d405
carllos39 <carllos2005@outlook.com.br>
2025-05-15 14:41:41 -0300

    atualizar

--- 05/pizzaria/Conexao.php
+++ 05/pizzaria/Conexao.php
@@ -1,15 +1,12 @@
-<?php
+<?php 
+class Conexao{
 
-// SINGLETON
-class Conexao {
     private static $bd = null;
-
-    public static function getBd() {
-         // return new PDO("mysql:host=localhost;dbname=pizzaria_senac", 'root', '');
-        if(self::$bd === null) {
-            self::$bd = new PDO("mysql:host=localhost;dbname=pizzaria_senac", 'root', '');
-            return self::$bd;
-        }
+    public static function getBd(){
+  if(self::$bd === null){
+  self::$bd = new PDO("mysql:host=localhost;dbname=pizzaria_senac","root","");
+  }
+  return self::$bd;
     }
 }
 ?>
--- 05/pizzaria/pizzaDao.php
+++ 05/pizzaria/pizzaDao.php
@@ -1,45 +1,42 @@
-<?php 
-require 'pizza.php';
+<?php
+require 'Conexao.php';
+require 'Pizza.php';
 
-    class PizzaDao
+class PizzaDAO
+{
+    private $db;
+
+    public function __construct()
     {
-        private $db;
-    
-        public function __construct()
-        {
-            $this->db = Conexao::getBd();
-        }
-    
-        public function getAll()
-        {        
-            $stmt = $this->db->query("SELECT * FROM pizza");
-            $pizzas = [];
-    
-            while ($row = $stmt->fetch((PDO::FETCH_ASSOC))) {
-                $pizzas[] = new Pizza(
-                    $row['id'],
-                    $row['sabor'],
-                    $row['tamanho'],
-                    $row['preco']
-                );
-            }
-            return $pizzas;
-        }
-
-        public function create(Pizza $pizza) {
-            $stmt = $this->db->prepare("INSERT INTO pizza (sabor, tamanho, preco)
-                VALUES (:sabor, :tamanho, :preco)");
-    
-            $sabor = $pizza->getSabor();
-            $tamanho = $pizza->getTamanho();
-            $preco = $pizza->getPreco();
-    
-            $stmt->bindParam(':sabor', $sabor);
-            $stmt->bindParam(':tamanho', $tamanho );
-            $stmt->bindParam(':preco', $preco);
-            $stmt->execute();
-        }
+        $this->db = Conexao::getBd();
     }
 
+    public function getAll()
+    {
+        $stmt = $this->db->query("SELECT * FROM pizza");
+        $pizza = [];
+        while ($row = $stmt->fetch((PDO::FETCH_ASSOC))) {
+            $pizza[] = new Pizza(
+                $row['id'],
+                $row['sabor'],
+                $row['tamanho'],
+                $row['preco']
+            );
+        }
+        return $pizza;
+    }
+    public function create(Pizza $pizza)
+    {
+        $stmt = $this->db->prepare("INSERT INTO pizza(sabor,tamanho,preco)
+        VALUES(:sabor,:tamanho,:preco)");
 
-?>
+        $sabor = $pizza->getSabor();
+        $tamanho = $pizza->getTamanho();
+        $preco = $pizza->getPreco();
+
+        $stmt->bindParam('sabor', $sabor);
+        $stmt->bindParam('tamanho', $tamanho);
+        $stmt->bindParam('preco', $preco);
+        $stmt-> execute();
+    }
+}
