0329cda113f7b6b394c9d83f5632a8ac1de47f3a
carllos39 <140573083+carllos39@users.noreply.github.com>
2025-05-21 12:06:28 -0300

    atualizar

--- 06/ContatoDAO.php
+++ 06/ContatoDAO.php
@@ -10,32 +10,47 @@ public function __construct()
 }
 
 public function getAll(){
-    $sql="SELECT* FROM contatos";
-    $contatos=[];
+    $sql="SELECT* FROM contato";
+    $contato=[];
    $stmt = $this->db->query($sql);
 
    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
   
-  $contatos[] = new Contato($row['id'],$row['nome'],$row['telefone'],$row['email'],$row['endereco']);
+  $contato[] = new Contato($row['id'],$row['nome'],$row['telefone'],$row['email'],$row['endereco']);
    }
-   return $contatos;
+   return $contato;
 }
 public function getById(int $id): ?Contato
 {
 
-  $stmt = $this->db->prepare("SELECT * FROM agenda.contatos WHERE id = :id");
+  $stmt = $this->db->prepare("SELECT * FROM contato WHERE id = :id");
   $stmt->bindParam(':id', $id);
   $stmt->execute();
   $row = $stmt->fetch(PDO::FETCH_ASSOC);
   return $row ? new Contato($row['id'], $row['nome'], $row['telefone'], $row['email'], $row['endereco']) :null;     
 }
-public function create(Contato $contatos){
- $sql="INSERT INTO contatos(nome,telefone,email,endereco) VALUES(:nome,:telefone,:email,:endereco)";
+public function create(Contato $contato){
+ $sql="INSERT INTO contato(nome,telefone,email,endereco) VALUES(:nome,:telefone,:email,:endereco)";
  $stmt= $this->db->prepare($sql);
- $nome = $contatos->getNome();
- $telefone = $contatos->getTelefone();
- $email = $contatos->getEmail();
- $endereco = $contatos->getEndereco();
+ $nome = $contato->getNome();
+ $telefone = $contato->getTelefone();
+ $email = $contato->getEmail();
+ $endereco = $contato->getEndereco();
+ $stmt->bindParam(':nome',$nome);
+ $stmt->bindParam(':telefone',$telefone);
+ $stmt->bindParam(':email',$email);
+ $stmt->bindParam(':endereco',$endereco);
+ $stmt->execute();
+}
+public function update(Contato $contato){
+ $sql="UPDATE contato SET nome=:nome,telefone=:telefone,email=:email,endereco=:endereco WHERE id=:id";
+ $stmt= $this->db->prepare($sql);
+  $id = $contato->getId();
+ $nome = $contato->getNome();
+ $telefone = $contato->getTelefone();
+ $email = $contato->getEmail();
+ $endereco = $contato->getEndereco();
+  $stmt->bindParam(':id',$id);
  $stmt->bindParam(':nome',$nome);
  $stmt->bindParam(':telefone',$telefone);
  $stmt->bindParam(':email',$email);
@@ -44,7 +59,7 @@ public function create(Contato $contatos){
 }
 public function delete(int $id): void
 {
-    $stmt = $this->db->prepare("DELETE FROM contatos WHERE id = :id");
+    $stmt = $this->db->prepare("DELETE FROM contato WHERE id = :id");
     $stmt->bindParam(':id', $id);
     $stmt->execute();
 }
--- 06/contato_form.php
+++ 06/contato_form.php
@@ -2,7 +2,29 @@
 require_once 'ContatoDAO.php';
 $dao = new ContatoDAO();
 
-if(isset($_POST['nome']) && isset($_POST['telefone']) && isset($_POST['email']))
+$contato=null; //Contato para edição
+
+//Editar contato
+if(isset($_GET['id'])){
+$contato=$dao->getById($_GET['id']);
+}
+//Salvar a edição de contato
+if(isset($_POST['id'])){
+
+    $endereco =null;
+if(isset($_POST['endereco'])){
+
+    $endereco = $_POST['endereco'];
+}
+   $contato = new Contato($_POST['id'], $_POST['nome'], $_POST['telefone'],$_POST['email'], $endereco);
+    $dao->update($contato);
+
+      header("Location: index.php");
+
+      exit();
+
+
+} else if(isset($_POST['nome']) && isset($_POST['telefone']) && isset($_POST['email']))
 {
     $endereco = null;
     if(isset($_POST['endereco']))
@@ -10,12 +32,12 @@ if(isset($_POST['nome']) && isset($_POST['telefone']) && isset($_POST['email']))
         $endereco = $_POST['endereco'];
     }
 
-    $contato = new Contato(null, $_POST['nome'], $_POST['telefone'],$_POST['email'], $endereco);
+    $contato = new Contato(null, $_POST['nome'], $_POST['telefone'], $_POST['email'], $endereco);
     $dao->create($contato);
 
     header("Location: index.php");
+    exit();
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -25,27 +47,30 @@ if(isset($_POST['nome']) && isset($_POST['telefone']) && isset($_POST['email']))
     <title>Cadastrar Contato</title>
 </head>
 <body>
-<h2>Cadastrar Novo Contato:</h2>
+<h2><?=$contato?"Editar Contato" :"Novo Contato"?></h2>
 
 <form action="contato_form.php" method="post">
+    <?php if($contato): ?>
+    <input type="hidden" name="id"  value="<?=$contato->getId()?>">
+    <?php endif;?>
     <div>
     <label>Nome:</label>
-    <input type="text" name="nome" required>
+    <input type="text" name="nome" required value="<?= $contato ?$contato->getNome():''?>">
     </div>
     <div>
     <label>Telefone:</label>
-    <input type="text" name="telefone" required>
+    <input type="text" name="telefone" required value="<?=$contato? $contato->getTelefone():''?>">
     </div>
     <div>
     <label>Email:</label>
-    <input type="text" name="email" required>
+    <input type="text" name="email" required value="<?=$contato? $contato->getEmail():''?>">
     </div>
     <div>
     <label>Endereco:</label>
-    <input type="text" name="endereco">
+    <input type="text" name="endereco" value="<?=$contato?$contato->getEndereco():''?>">
     </div>
     <button type="submit">Salvar</button>
 </form>
-<a href="index.php">Listas</a><br>
+<a href="index.php">Cancelar</a><br>
 </body>
 </html>
