81d2910d557d670c8addc28f1ff196b964f7bbc3
carllos39 <carllos2005@outlook.com.br>
2025-05-28 09:49:01 -0300

    atualizar

--- 06/ContatoDAO.php
+++ 06/ContatoDAO.php
@@ -1,7 +1,6 @@
 <?php 
 require_once 'Database.php';
 require_once 'Contato.php';
-
  class ContatoDAO{
 private $db;
 
@@ -12,26 +11,64 @@ public function __construct()
 
 public function getAll(){
     $sql="SELECT* FROM contatos";
-     $contatos=[];
+    $contatos=[];
    $stmt = $this->db->query($sql);
 
    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-   
-  $contatos[] = new Contato($row['id'],$row['nome']);
+  
+  $contatos[] = new Contato($row['id'],$row['nome'],$row['telefone'],$row['email'],$row['endereco']);
    }
    return $contatos;
 }
+public function getById(int $id): ?Contato
+{
+
+  $stmt = $this->db->prepare("SELECT * FROM agenda.contatos WHERE id = :id");
+  $stmt->bindParam(':id', $id);
+  $stmt->execute();
+  $row = $stmt->fetch(PDO::FETCH_ASSOC);
+  return $row ? new Contato($row['id'], $row['nome'], $row['telefone'], $row['email'], $row['endereco']) :null;     
+}
 public function create(Contato $contatos){
- $sql="INSERT INTO contatos(nome) VALUES(:nome)";
+ $sql="INSERT INTO contatos(nome,telefone,email,endereco) VALUES(:nome,:telefone,:email,:endereco)";
  $stmt= $this->db->prepare($sql);
  $nome = $contatos->getNome();
+ $telefone = $contatos->getTelefone();
+ $email = $contatos->getEmail();
+ $endereco = $contatos->getEndereco();
  $stmt->bindParam(':nome',$nome);
+ $stmt->bindParam(':telefone',$telefone);
+ $stmt->bindParam(':email',$email);
+ $stmt->bindParam(':endereco',$endereco);
  $stmt->execute();
+}
+public function update(Contato $contatos){
+  $sql="UPDATE contatos SET nome=:nome,telefone=:telefone,email=:email,endereco=:endereco WHERE id=:id";
+  $stmt =$this->db->prepare($sql);
+
+  $id = $contatos->getId();
+  $nome = $contatos->getNome();
+  $telefone = $contatos->getTelefone();
+  $email = $contatos->getEmail();
+  $endereco = $contatos->getEndereco();
+  
+  $stmt = $this->db->prepare($sql);
+  $stmt->bindParam(':id', $id);
+  $stmt->bindParam(':nome', $nome);
+  $stmt->bindParam(':telefone', $telefone);
+  $stmt->bindParam(':email', $email);
+  $stmt->bindParam(':endereco', $endereco);
+
+  $stmt->execute();
+}
+public function delete(int $id): void
+{
+    $stmt = $this->db->prepare("DELETE FROM contatos WHERE id = :id");
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
 }
 
  }
-
-
 //  // TESTE DE CLASSE
 // $cont1 = new Contato(null, "Batman");
 // $dao = new ContatoDAO();
--- 06/index.php
+++ 06/index.php
@@ -1,9 +1,8 @@
-<?php 
+<?php
+
 require_once 'ContatoDAO.php';
-
 $dao = new ContatoDAO();
-
-$contatos=$dao->getAll();
+$contatos = $dao->getAll();
 
 ?>
 
@@ -16,26 +15,26 @@ $contatos=$dao->getAll();
 </head>
 <body>
     <h2>Lista de Contatos</h2>
+
     <a href="contato_form.php">Cadastrar Contato</a><br>
-
-    <table border ="1" cellpadding="5">
+    
+    <table border="1" cellpadding="5">
         <tr>
-            <th>ID :</th>
-            <th>Nome:</th>
-            <th>Ações :</th>
+            <th>Nome</th>
+            <th>Telefone</th>
+            <th>Ações</th>
         </tr>
-        <?php foreach($contatos as $contato): ?>
-          <tr>
-           <td><?= $contato->getId()?></td>
-           <td><?= $contato->getNome()?></td>
-           <td>
-           <a href="#">Detalhes</a>
-            <a href="#">Editar</a>
-            <a href="#">Excluir</a>
-           </td>
-          </tr>
-        <?php endforeach;?>
-
+        <?php foreach ($contatos as $c): ?>
+            <tr>
+                <td><?= $c->getNome() ?></td>
+                <td><?= $c->getTelefone() ?></td>
+                <td>
+                    <a href="contato_ditails.php?id=<?= $c->getId() ?>">Detalhes</a>
+                    <a href="contato_form.php?id=<?= $c->getId() ?>">Editar</a>
+                    <a href="contato_delete.php?id=<?= $c->getId() ?>">Excluir</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
     </table>
 </body>
 </html>
